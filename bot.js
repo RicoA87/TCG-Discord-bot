88a91ac4864915de7444d7c59ba524e35e5a4ce9
RicoA87 <142189805+RicoA87@users.noreply.github.com>
2025-03-04 00:42:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const sqlite3 = require('sqlite3').verbose();
 require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-const db = new sqlite3.Database('./cards.db', (err) => {
+const db = new sqlite3.Database('/app/cards.db', (err) => {
     if (err) console.error(err.message);
     console.log('Connected to the SQLite database.');
 });
